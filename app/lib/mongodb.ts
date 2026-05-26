202b02a73605d1571092cd99d0b4185135b5f61e
Ice <mmmmmm@Ices-MacBook-Pro.local>
2026-05-26 17:43:27 +0800

    chapter 8

--- app/lib/mongodb.ts
+++ app/lib/mongodb.ts
@@ -1,4 +1,54 @@
+import dns from 'dns';
 import { MongoClient } from 'mongodb';
+
+// 学校/公司 DNS 屏蔽了 *.mongodb.net，用公共 DNS 接管 dns.lookup
+const resolver = new dns.Resolver();
+resolver.setServers(['8.8.8.8', '1.1.1.1']);
+
+function customLookup(
+  hostname: string,
+  options: dns.LookupOptions | ((err: NodeJS.ErrnoException | null, address: string, family: number) => void),
+  callback?: (err: NodeJS.ErrnoException | null, address: string, family: number) => void,
+) {
+  const cb = typeof options === 'function' ? options : callback!;
+  const opts = typeof options === 'object' ? options : {};
+  const family = opts.family || opts.hints || 0;
+  const all = opts.all || false;
+
+  const tasks: Promise<string[]>[] = [];
+  if (family !== 6) tasks.push(promisifyResolve(resolver.resolve4.bind(resolver), hostname).catch(() => []));
+  if (family !== 4) tasks.push(promisifyResolve(resolver.resolve6.bind(resolver), hostname).catch(() => []));
+
+  Promise.all(tasks).then((results) => {
+    const addresses = results.flatMap((addrs, i) =>
+      addrs.map(addr => ({ address: addr, family: (i === 0 && family !== 6) || family === 4 ? 4 : 6 }) as { address: string; family: 4 | 6 })
+    );
+    if (addresses.length === 0) {
+      const err = new Error(`getaddrinfo ENOTFOUND ${hostname}`) as NodeJS.ErrnoException & { hostname: string };
+      err.code = 'ENOTFOUND';
+      err.errno = -3008;
+      err.syscall = 'getaddrinfo';
+      err.hostname = hostname;
+      return all ? (cb as Function)(err) : cb(err, '', 0);
+    }
+    if (all) {
+      (cb as Function)(null, addresses);
+    } else {
+      cb(null, addresses[0].address, addresses[0].family);
+    }
+  }).catch((err) => (all ? (cb as Function)(err) : cb(err, '', 0)));
+}
+
+function promisifyResolve(fn: (hostname: string, cb: (err: NodeJS.ErrnoException | null, addresses: string[]) => void) => void, hostname: string): Promise<string[]> {
+  return new Promise((resolve, reject) => {
+    fn(hostname, (err, addresses) => {
+      if (err) reject(err);
+      else resolve(addresses);
+    });
+  });
+}
+
+(dns as any).lookup = customLookup;
 
 const uri = process.env.MONGODB_URI!;
 const client = new MongoClient(uri);
