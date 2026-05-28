21e446c31d7229a34dbca57c42c2810b986e3629
ice886 <liuyang20211001@icloud.com>
2026-05-28 16:10:38 +0800

    chapter 11 add

---
+++ app/lib/actions.ts
@@ -0,0 +1,37 @@
+'use server';
+
+import { z } from 'zod';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
+import { getDb } from './mongodb';
+
+
+const FormSchema = z.object({
+  id: z.string(),
+  customerId: z.string(),
+  amount: z.coerce.number(),
+  status: z.enum(['pending', 'paid']),
+  date: z.string(),
+})
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
+export async function createInvoice(formData: FormData) {
+  const { customerId, amount, status } = CreateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+  const amountInCents = amount * 100;
+  const date = new Date().toISOString().split('T')[0];
+
+  const db = await getDb();
+  await db.collection('invoices').insertOne({
+    id: crypto.randomUUID(),
+    customer_id: customerId,
+    amount: amountInCents,
+    status,
+    date,
+  });
+
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
+}
